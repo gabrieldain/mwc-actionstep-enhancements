7a5715dccfbb4a4b3aec04650819bc7cd1132a5d
gabrieldain <gabrieldain@gmail.com>
2024-07-30 13:19:55 +1000

    Create Multi-Select-Resizer.user.js

---
+++ Multi-Select-Resizer.user.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         Multi-Select Resizer
+// @description  Add drag & drop resize functionality to multi-select elements and double their default height
+// @namespace    Migrant Workers Centre
+// @match        *ap-southeast-2.actionstep.com/*
+// @grant        none
+// @version      0.3
+// @author       Gabriel Dain <gdain@migrantworkers.org.au>
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function makeResizable(element) {
+        const resizer = document.createElement('div');
+        resizer.className = 'resizer';
+        resizer.style.width = '10px';
+        resizer.style.height = '10px';
+        resizer.style.background = 'gray';
+        resizer.style.position = 'absolute';
+        resizer.style.right = '0';
+        resizer.style.bottom = '0';
+        resizer.style.cursor = 'se-resize';
+
+        element.style.position = 'relative';
+        element.style.resize = 'both';
+        element.style.overflow = 'auto';
+        element.appendChild(resizer);
+
+        resizer.addEventListener('mousedown', initResize, false);
+
+        function initResize(e) {
+            window.addEventListener('mousemove', Resize, false);
+            window.addEventListener('mouseup', stopResize, false);
+        }
+
+        function Resize(e) {
+            const newWidth = e.clientX - element.getBoundingClientRect().left;
+            const newHeight = e.clientY - element.getBoundingClientRect().top;
+
+            if (newWidth > 0) element.style.width = newWidth + 'px';
+            if (newHeight > 0) element.style.height = newHeight + 'px';
+        }
+
+        function stopResize(e) {
+            window.removeEventListener('mousemove', Resize, false);
+            window.removeEventListener('mouseup', stopResize, false);
+        }
+    }
+
+    // Find all multi-select elements, double their default height, and make them resizable
+    const multiSelects = document.querySelectorAll('select[multiple]');
+    multiSelects.forEach(element => {
+        const defaultHeight = element.offsetHeight;
+        element.style.height = (defaultHeight * 2) + 'px';
+        makeResizable(element);
+    });
+})();
