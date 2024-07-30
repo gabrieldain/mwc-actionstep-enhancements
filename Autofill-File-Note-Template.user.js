2f7f25dac26ebad4df2a89a346eb8545841d3620
gabrieldain <gabrieldain@gmail.com>
2024-07-30 12:53:03 +1000

    Increase version number

--- Autofill-File-Note-Template.user.js
+++ Autofill-File-Note-Template.user.js
@@ -4,7 +4,7 @@
 // @namespace    Migrant Workers Centre
 // @match        *ap-southeast-2.actionstep.com/*
 // @grant        none
-// @version      0.931
+// @version      0.932
 // @author       Gabriel Dain <gdain@migrantworkers.org.au>
 // @downloadURL  https://github.com/gabrieldain/mwc-actionstep-enhancements/raw/main/Autofill-File-Note-Template.user.js
 // @updateURL    https://github.com/gabrieldain/mwc-actionstep-enhancements/raw/main/Autofill-File-Note-Template.user.js
